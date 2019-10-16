8f3117005d1c62eae040aaf7cd11075db37ac808
mrb76 <mrbakhtiari4@yahoo.com>
2019-10-16 11:20:19 +0330

    add test component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Test func="change" disable="tex"  msg="<a href='http://google.com'>aaaaa</a>"/>
   </div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
+  },
+  methods:{
+    change(){
+      this.text={"1":1,"2":2};
+      Test.disabled=this.text
+      }
   }
 }
+let text=1;
+
 </script>
 
 <style>
---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p v-for="item in disable" :key="item">
+            {{item}}
+        </p>
+        <button v-on:click="ch">a</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Test',
+    props:{
+        msg:String,
+        disable:String,
+        func:Function
+    }
+}
+</script>
+
+<style>
+
+</style>
